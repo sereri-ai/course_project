e4d4f3141f9e96db2c156981f33d3698d634241f
sereri-ai <zrj201@gmail.com>
2023-01-08 17:46:09 -0500

    Back-ground image implementation v2.0

--- src/Home.js
+++ src/Home.js
@@ -7,7 +7,7 @@ class Home extends Component {
         <h2>Welcome to the Twisted Crab</h2>
         <p>Fresh seafood straight from the water to the kitchen!</p>
         <p>Orders available for pickup and delivery!</p>
-        <img src={require('./tc.jpg')} alt="ENTREE"></img>
+        <img src={require('./tc.jpg')} alt="ENTREE"></img>  
       </div>
     );
   }
